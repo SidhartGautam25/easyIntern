e08b4700b052e3720c434e33a1f4ef17f4706eb4
Sidharth Gautam <navneetnishchal1420@gmail.com>
2026-06-19 20:31:26 +0530

    logging also tested for local environment

--- updatedeasy/server/src/routes/auth.ts
+++ updatedeasy/server/src/routes/auth.ts
@@ -8,5 +8,6 @@ const authController = new AuthController();
 // Apply rate limiter on password reset OTP requests
 router.post('/forgot-password', rateLimiter, authController.requestPasswordResetOtp);
 router.post('/reset-password', rateLimiter, authController.resetPasswordWithOtp);
+router.post('/log-event', authController.logEvent);
 
 export const authRoutes = router.routes();
